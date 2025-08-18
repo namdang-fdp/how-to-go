f5534abb8436172008eaff6c7df522d331050c90
namdang-fdp <namdangcoder@gmail.com>
2025-08-18 12:14:53 +0700

    feat: add variables

---
+++ 4.Variables/main.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	myvar1, myvar2, myvar3 := 800, "Nam Dang", 47.56
+	fmt.Printf("The value of var1 is: %d", myvar1)
+	fmt.Printf("The value of var2 is: %s\n", myvar2)
+	fmt.Printf("The value of var3 is: %f\n", myvar3)
+}
