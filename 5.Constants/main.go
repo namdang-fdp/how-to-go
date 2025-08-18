d7edda4b3514c2513df6da2ed40cc238d5310f1a
namdang-fdp <namdangcoder@gmail.com>
2025-08-18 12:29:52 +0700

    feat: add constants

---
+++ 5.Constants/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+const (
+	GFG     = "GeeksforGeeks"
+	Correct = true
+	PI      = 3.14
+)
+
+func main() {
+	fmt.Println("Value of GFG: ", GFG)
+	fmt.Println("Value of Correct: ", Correct)
+	fmt.Println("Value of PI: ", PI)
+}
