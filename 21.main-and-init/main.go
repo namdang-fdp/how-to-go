c257a03dd9b4590895e8c6e022158ab48cdd75f3
namdang-fdp <namdangcoder@gmail.com>
2025-11-06 10:17:47 +0700

    feat: add main-init

---
+++ 21.main-and-init/main.go
@@ -0,0 +1,7 @@
+package main
+
+// main is the special package that used
+// with the program that are exccutable
+// main() func is the entry point. Do not receive nor return anything
+
+// init func is the func that exec when the package is init
